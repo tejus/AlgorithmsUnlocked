50d25a7b56d298e4fe6e820ad11f4736c69c7bd9
tejus <tejus.k.v@gmail.com>
2019-05-29 20:45:52 +0530

    Code cleanup

--- src/Searching/LinearSearch.kt
+++ src/Searching/LinearSearch.kt
@@ -1,6 +1,6 @@
 package Searching
 
-fun <E> linearSearch(unsortedList: List<E>, size: Int, itemToFind: E): Int where E : Comparable<E>{
+fun <E> linearSearch(unsortedList: List<E>, size: Int, itemToFind: E): Int where E : Comparable<E> {
     var answer = -1
     for (i in 0 until size) {
         if (unsortedList.get(i) == itemToFind) answer = i
